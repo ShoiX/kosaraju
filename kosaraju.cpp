bc32d8d7996f2fae9edaeddb7080e3ff61cd900e
ShoiX <jerrycoalaba@gmail.com>
2018-05-15 08:24:56 +0800

    Modified the storing of adjacent vertices on the vertex class and finished the identifying of all SCCs

--- kosaraju.cpp
+++ kosaraju.cpp
@@ -13,6 +13,7 @@
 #include <fstream>
 #include <stdlib.h>	
 #define INITLIMIT 900000
+#define top 5
 using namespace std;
 	
 class vertex
@@ -20,8 +21,8 @@ class vertex
 public:
 	int label = 0;	// name of the vertex
 	// vector for vertices adjacent (in and out)
-	vector<int> outward;
-	vector<int> inward;
+	vector<vertex*> outward;
+	vector<vertex*> inward;
 	int order;
 	bool first_pass = false;
 	bool second_pass = false;
@@ -33,25 +34,55 @@ public:
 	}
 };
 
+class scc
+{
+public:
+	std::vector<vertex*> components;
+
+	scc()
+	{
+		components.reserve(200);
+	}
+
+};
+
 int finish_time = 0;
 vertex** order;
 
-void DFS1(int cur, vector<vertex> &vertices)
+
+// first pass of DFS
+void DFS1(vertex* cur)
 {
-	vertices[cur-1].first_pass = true;
-	for (int i = 0, j = vertices[cur-1].inward.size(); i < j; i++)
+	cur->first_pass = true;
+	for (int i = 0, j = cur->inward.size(); i < j; i++)
 	{
-		int cur_vec = vertices[cur - 1].inward[i];
-		if (!vertices[cur_vec - 1].first_pass)
+		vertex* next_vec = cur->inward[i];
+		if (!next_vec->first_pass)
 		{
-			DFS1(cur_vec, vertices);
+			DFS1(next_vec);
 		}
 	}
 	// set the finishing time
-	vertices[cur - 1].order = finish_time;
-	order[finish_time] = &vertices[cur - 1];
+	cur->order = finish_time;
+	order[finish_time] = cur;
 	finish_time += 1;
 	return;
+}
+
+void DFS2(vertex* cur, scc* tmp)
+{
+	cur->second_pass = true;
+	tmp->components.push_back(cur);
+	for (int i = 0, j = cur->outward.size(); i < j; i++)
+	{
+		vertex* next_vec = cur->outward[i];
+		if (!next_vec->second_pass)
+		{
+			DFS2(next_vec, tmp);	
+		}
+	}
+	return;
+
 }
 
 /* DUMP subroutine*/
@@ -62,11 +93,11 @@ void gen_dump(vector<vertex> &vertices, int n)
 	{
 		cout<<"vertex "<<vertices[i].label<<" order: "<<vertices[i].order<<endl<<"outward: ";
 		for (int k = 0, l = vertices[i].outward.size(); k < l; k++)
-			cout<<vertices[i].outward[k]<<" ";
+			cout<<vertices[i].outward[k]->label<<" ";
 		cout<<endl<<"inward: ";
 
 		for (int m = 0, n = vertices[i].inward.size(); m < n; m++)
-			cout<<vertices[i].inward[m]<<" ";
+			cout<<vertices[i].inward[m]->label<<" ";
 		cout<<endl;
 	}
 }
@@ -75,7 +106,22 @@ void order_dump(vector<vertex> &vertices, int n)
 {
 	for (int i = 0; i < n; i++)
 	{
-		cout<<"pos "<<i<<": "<<order[i]->label<<endl;
+		cout<<"pos "<<i + 1<<": "<<order[i]->label<<endl;
+	}
+}
+
+void scc_dump(vector<scc> &SCCs)
+{
+	int len = SCCs.size();
+	cout<<"number of SCCs: "<<len<<endl;
+	for (int i = 0; i < len; i++)
+	{
+		cout<<"scc no. "<<i + 1<<": ";
+		for (int j = 0, k = SCCs[i].components.size(); j < k; j++)
+		{
+			cout<<SCCs[i].components[j]->label<<" ";
+		}
+		cout<<endl;
 	}
 }
 
@@ -83,7 +129,7 @@ int main(int argc, char* argv[])
 {
 	if (argc < 2)
 	{
-		cout<<"usage: ./kosaraju file.txt [1/2]\n";
+		cout<<"usage: ./kosaraju file.txt [1/2/3/4]\n";
 		return 1;	
 	}
 	std::vector<vertex> vertices;
@@ -116,8 +162,8 @@ int main(int argc, char* argv[])
 		vertices[a-1].label = (vertices[a - 1].label == 0) ? a : vertices[a-1].label;
 		vertices[b-1].label = (vertices[b - 1].label == 0) ? b : vertices[b-1].label; 
 
-		vertices[a-1].outward.push_back(b);
-		vertices[b-1].inward.push_back(a);
+		vertices[a-1].outward.push_back(&vertices[b -  1]);
+		vertices[b-1].inward.push_back(&vertices[a - 1]);
 	}
 
 	n = vertices.size();
@@ -127,9 +173,45 @@ int main(int argc, char* argv[])
 	{
 		if (!vertices[i - 1].first_pass)
 		{
-			DFS1(i, vertices);
+			DFS1(&vertices[i - 1]);
 		}
 	}
+
+	// second DFS pass
+	std::vector<scc> SCCs;
+	SCCs.reserve(200000);
+	scc* toplist[top];
+	int ctr = 0;
+	for (int i = n - 1; i >= 0; i--)
+	{
+		if (!order[i]->second_pass)
+		{
+			scc tmp;
+			DFS2(order[i], &tmp);
+			SCCs.push_back(tmp);
+
+			int SCCs_size = SCCs.size();
+
+			// build the list of the largest SCCs of the graph
+			if(ctr < top)
+			{
+				toplist[ctr] = &SCCs[SCCs_size - 1];
+				ctr++;
+			}
+			else
+			{
+				for (int j = 0; j < top; j++)
+				{
+					if (tmp.components.size() > toplist[i]->components.size())
+					{
+						toplist[i] = &SCCs[SCCs_size - 1];
+						break;
+					}
+				}
+			}
+		}
+	}
+
 	if (argc == 3)
 	{
 		if (atoi(argv[2]) == 1)
@@ -140,7 +222,16 @@ int main(int argc, char* argv[])
 		{
 			order_dump(vertices, n);
 		}
+		else if (atoi(argv[2]) == 3)
+		{
+			scc_dump(SCCs);
+		}
+		else if (atoi(argv[2]) == 4)
+		{
+			gen_dump(vertices, n);
+			order_dump(vertices, n);
+		}
 	}
-
+	delete[] order;
 	f.close();
 }
